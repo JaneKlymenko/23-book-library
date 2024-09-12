bcf337853e7e64c980c6f23fbefb80cadfb7d929
JaneKlymenko <103850306+JaneKlymenko@users.noreply.github.com>
2024-09-12 18:43:15 +0300

    Added components and redux

---
+++ frontend/src/components/BookForm/BookForm.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBook } from "../../redux/books/actionCreators";
+import "./BookForm.css";
+
+const BookForm = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && author) {
+      const book = {
+        title,
+        author,
+      };
+      console.log(addBook(book));
+      dispatch(addBook(book));
+      setTitle("");
+      setAuthor("");
+    }
+  };
+  return (
+    <div className="app-block book-forn">
+      <h2>Add new book</h2>
+      <form onSubmit={handleSubmit} className="book-form">
+        <div>
+          <label htmlFor="title">Title: </label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="author">Author: </label>
+          <input
+            type="text"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+};
+
+export default BookForm;
---
+++ frontend/src/components/Filter/Filter.js
@@ -0,0 +1,11 @@
+import "./Filter.css";
+
+const Filter = () => {
+  return (
+    <div className="app-block filtert">
+      <h2>Filter</h2>
+    </div>
+  );
+};
+
+export default Filter;
