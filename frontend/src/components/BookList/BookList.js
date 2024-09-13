110072a908bf2ffac888eca056692c4032567418
JaneKlymenko <103850306+JaneKlymenko@users.noreply.github.com>
2024-09-13 19:37:21 +0300

    func delet books added

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,8 +1,33 @@
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { deleteBook } from "../../redux/books/actionCreators";
 import "./BookList.css";
+
 const BookList = () => {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const handleDeleteBook = (id) => {
+    dispatch(deleteBook(id));
+  };
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
+      {books.length === 0 ? (
+        <p>No books available</p>
+      ) : (
+        <ul>
+          {books.map((book, i) => (
+            <li key={book.id}>
+              <div className="book-info">
+                {++i}. <b>{book.title}</b> by <i>{book.author}</i>
+              </div>
+              <div className="book-actions">
+                <button onClick={() => handleDeleteBook(book.id)}>Del</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
