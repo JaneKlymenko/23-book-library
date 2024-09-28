0427eec1fafc3b880160ba2cee3fce72e6a3af02
JaneKlymenko <103850306+JaneKlymenko@users.noreply.github.com>
2024-09-28 17:08:39 +0300

    filter by title created

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -3,9 +3,11 @@ import { useDispatch } from "react-redux";
 import { deleteBook, toggleFavorite } from "../../redux/books/actionCreators";
 import { RiBookmark3Fill, RiBookmark3Line } from "react-icons/ri";
 import "./BookList.css";
+import { selectTitleFilter } from "../../redux/slices/filterSlice";
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const titleFilter = useSelector(selectTitleFilter);
   const dispatch = useDispatch();
   const handleDeleteBook = (id) => {
     dispatch(deleteBook(id));
@@ -14,6 +16,14 @@ const BookList = () => {
   const handleToggleFavorite = (id) => {
     dispatch(toggleFavorite(id));
   };
+
+  const filtredBooks = books.filter((book) => {
+    const matchesTitle = book.title
+      .toLowerCase()
+      .includes(titleFilter.toLowerCase());
+    // console.log({ title: book.title, matchesTitle });
+    return matchesTitle;
+  });
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -21,7 +31,7 @@ const BookList = () => {
         <p>No books available</p>
       ) : (
         <ul>
-          {books.map((book, i) => (
+          {filtredBooks.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
                 {++i}. <b>{book.title}</b> by <i>{book.author}</i>
--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,9 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectTitleFilter,
+  setTitleFilter,
+} from "../../redux/slices/filterSlice";
+
 import "./Filter.css";
 
 const Filter = () => {
+  const dispatch = useDispatch();
+  const titleFilter = useSelector(selectTitleFilter);
+
+  const handletitleFilterChange = (e) => {
+    dispatch(setTitleFilter(e.target.value));
+    console.log(titleFilter);
+  };
+
   return (
     <div className="app-block filtert">
-      <h2>Filter</h2>
+      <div className="filter-group">
+        <input
+          type="text"
+          value={titleFilter}
+          placeholder="Filter by title...."
+          onChange={handletitleFilterChange}
+        />
+      </div>
     </div>
   );
 };
